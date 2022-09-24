9bbdfd8621eec263e9c3f613542dcf37b1d8039c
kajal-dinesh <tdkajal@gmail.com>
2022-09-24 14:22:23 +0530

    interviewtask1

---
+++ app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+
+
+const routes: Routes = [
+  {
+    path:"",component:LoginComponent
+  },
+  {
+    path:"home",component:HomeComponent
+  },
+  {
+    path:"about",component:AboutComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
